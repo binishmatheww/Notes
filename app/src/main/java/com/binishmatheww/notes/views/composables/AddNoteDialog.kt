e432be234bfbf1adc0ec83f757b155f7de6004d9
binishmatheww <binishmatheww@gmail.com>
2022-10-17 20:06:48 +0530

    Fixed dialog box issue.

--- app/src/main/java/com/binishmatheww/notes/views/composables/AddNoteDialog.kt
+++ app/src/main/java/com/binishmatheww/notes/views/composables/AddNoteDialog.kt
@@ -7,19 +7,24 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.binishmatheww.notes.core.themes.AppTheme
 import com.binishmatheww.notes.R
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun AddNoteDialog(
     modifier: Modifier = Modifier,
@@ -39,49 +44,68 @@ fun AddNoteDialog(
             properties = DialogProperties(
                 dismissOnBackPress = true,
                 dismissOnClickOutside = false,
+                usePlatformDefaultWidth = false
             ),
 
             ) {
 
             Card(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(
+                        all = 14.dp
+                    ),
                 elevation = 8.dp,
                 shape = RoundedCornerShape(12.dp),
             ) {
 
-                Column(
+                ConstraintLayout(
                     modifier = modifier,
                 ) {
 
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .background(MaterialTheme.colorScheme.background.copy(alpha = 0.05f))
-                            .padding(bottom = 10.dp),
-                        horizontalArrangement = Arrangement.Center
-                    ) {
-                        Text(
-                            text = context.getString(R.string.addNote),
-                            modifier = Modifier
-                                .padding(top = 12.dp),
-                            style = MaterialTheme.typography.labelLarge.copy( color = MaterialTheme.colorScheme.primary ),
-                            textAlign = TextAlign.Center
-                        )
-                    }
-
-                    Spacer(modifier = Modifier.height(20.dp))
+                    val (
+                        dialogTitleConstraint,
+                        noteTitleConstraint,
+                        noteTitleInputConstraint,
+                        noteDescriptionConstraint,
+                        noteDescriptionInputConstraint,
+                        saveButtonConstraint,
+                    ) = createRefs()
 
                     Text(
+                        modifier = Modifier
+                            .constrainAs(dialogTitleConstraint){
+                                top.linkTo(parent.top, 14.dp)
+                                start.linkTo(parent.start)
+                                end.linkTo(parent.end)
+                                width = Dimension.fillToConstraints
+                            },
+                        text = context.getString(R.string.addNote),
+                        style = MaterialTheme.typography.displaySmall.copy( color = MaterialTheme.colorScheme.primary ),
+                        textAlign = TextAlign.Center
+                    )
+
+                    Text(
+                        modifier = Modifier
+                            .constrainAs(noteTitleConstraint){
+                                top.linkTo(dialogTitleConstraint.bottom, 8.dp)
+                                start.linkTo(parent.start, 14.dp)
+                            },
                         text = context.getString(R.string.title),
-                        modifier = Modifier.padding(start = 14.dp),
                         style = MaterialTheme.typography.labelLarge.copy( color = MaterialTheme.colorScheme.primary )
                     )
 
-                    Spacer(modifier = Modifier.height(5.dp))
-
                     TextInputField(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .requiredHeight(50.dp),
+                            .constrainAs(noteTitleInputConstraint){
+                                top.linkTo(noteTitleConstraint.bottom, 8.dp)
+                                start.linkTo(parent.start, 14.dp)
+                                end.linkTo(parent.end, 14.dp)
+                            }
+                            .heightIn(
+                                min = 40.dp,
+                                max = 120.dp
+                            ),
                         text = title.value,
                         onValueChange = {
                             title.value = it
@@ -94,20 +118,27 @@ fun AddNoteDialog(
                         }
                     )
 
-                    Spacer(modifier = Modifier.height(10.dp))
-
                     Text(
+                        modifier = Modifier
+                            .constrainAs(noteDescriptionConstraint){
+                                top.linkTo(noteTitleInputConstraint.bottom, 8.dp)
+                                start.linkTo(parent.start, 14.dp)
+                            },
                         text = context.getString(R.string.description),
-                        modifier = Modifier.padding(start = 14.dp),
                         style = MaterialTheme.typography.labelLarge.copy( color = MaterialTheme.colorScheme.primary )
                     )
 
-                    Spacer(modifier = Modifier.height(5.dp))
-
                     TextInputField(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .requiredHeight(50.dp),
+                            .constrainAs(noteDescriptionInputConstraint){
+                                top.linkTo(noteDescriptionConstraint.bottom, 8.dp)
+                                start.linkTo(parent.start, 14.dp)
+                                end.linkTo(parent.end, 14.dp)
+                            }
+                            .heightIn(
+                                min = 40.dp,
+                                max = 120.dp
+                            ),
                         text = description.value,
                         onValueChange = {
                             description.value = it
@@ -120,35 +151,39 @@ fun AddNoteDialog(
                         }
                     )
 
-                    Spacer(modifier = Modifier.height(5.dp))
+                    Button(
+                        modifier = Modifier.constrainAs(saveButtonConstraint){
+                            top.linkTo(noteDescriptionInputConstraint.bottom, 8.dp)
+                            bottom.linkTo(parent.bottom, 14.dp)
+                            end.linkTo(parent.end, 14.dp)
+                        },
+                        onClick = {
 
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 10.dp, bottom = 20.dp, end = 20.dp),
-                        horizontalArrangement = Arrangement.End
-                    ) {
-                        Text(
-                            text = context.getString(R.string.save),
-                            modifier = Modifier
-                                .clickable {
-                                    if ( title.value.isNotBlank() ) {
-                                        onSave.invoke(title.value, description.value)
-                                    } else {
-                                        Toast
-                                            .makeText(
-                                                context,
-                                                context.getText(R.string.enterNoteTitle),
-                                                Toast.LENGTH_SHORT
-                                            )
-                                            .show()
-                                    }
-                                },
-                            style = MaterialTheme.typography.titleMedium.copy(
-                                color = if ( title.value.isNotBlank() ) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.primary.copy(alpha = 0.5f)
+                            if ( title.value.isNotBlank() ) {
+                                onSave.invoke(title.value, description.value)
+                            } else {
+                                Toast
+                                    .makeText(
+                                        context,
+                                        context.getText(R.string.enterNoteTitle),
+                                        Toast.LENGTH_SHORT
+                                    )
+                                    .show()
+                            }
+
+                        },
+                        content = {
+
+                            Text(
+                                text = context.getString(R.string.save),
+                                modifier = Modifier,
+                                style = MaterialTheme.typography.titleMedium.copy(
+                                    color = if ( title.value.isNotBlank() ) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.5f)
+                                )
                             )
-                        )
-                    }
+
+                        }
+                    )
 
                 }
 
