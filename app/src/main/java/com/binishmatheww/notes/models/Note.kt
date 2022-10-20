be9e42ea54acfd5dd1c43f30cf36bedcb1dc64a1
binishmatheww <binishmatheww@gmail.com>
2022-10-20 20:32:44 +0530

    Added support functions to convert the class to JsonObject and vice versa.

--- app/src/main/java/com/binishmatheww/notes/models/Note.kt
+++ app/src/main/java/com/binishmatheww/notes/models/Note.kt
@@ -2,6 +2,33 @@ package com.binishmatheww.notes.models
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+
 
 @Entity(tableName = "notes")
-data class Note(@PrimaryKey val id : Long, val title : String, val description : String, val colorId : Int, val modifiedAt : Long)
+data class Note(
+    @PrimaryKey val id: Long,
+    val title: String,
+    val description: String,
+    val colorId: Int,
+    val modifiedAt: Long
+) {
+
+    companion object {
+
+        fun fromString( string: String ) = Gson().fromJson(string, Note::class.java)
+
+    }
+
+    fun toJsonObject() = JsonObject().apply {
+        addProperty("id",id)
+        addProperty("title",title)
+        addProperty("description",description)
+        addProperty("colorId",colorId)
+        addProperty("modifiedAt",modifiedAt)
+    }
+
+    override fun toString() = toJsonObject().toString()
+
+}
